3fde9cf959dfe7fdd14f1e56353a9a29977407d9
Tomas Perez <tom@0x101.com>
2013-09-22 23:20:52 +0400

    Adding the insertion sort algorithm.

---
+++ test/test.insertion-sort.js
@@ -0,0 +1,18 @@
+var expect = require('expect.js');
+require('../lib/algorithm/insertion-sort');
+
+describe('Insertion sort', function() {
+
+  it('Trivial cases', function() {
+    var insertionSort = new InsertionSort();
+    expect(insertionSort.solve([])).to.eql([]);
+    expect(insertionSort.solve([1])).to.eql([1]);
+    expect(insertionSort.solve([1, 2, 3])).to.eql([1, 2, 3]);
+  });
+
+  it('Non trivial cases', function() {
+    var insertionSort = new InsertionSort();
+    expect(insertionSort.solve([1, 4, 8, 9, 11, 15, 7])).to.eql([1, 4, 7, 8, 9, 11, 15]);
+  });
+
+});
