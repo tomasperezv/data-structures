827adf3eb1e3cd5bc4a6812f9b54afdd1567a77d
Tomas Perez <tom@0x101.com>
2013-10-06 01:55:19 +0400

    Adding support for double linked lists.

---
+++ test/test.double-linked-list.js
@@ -0,0 +1,42 @@
+var expect = require('expect.js');
+require('../lib/double-linked-list');
+
+describe('DoubleLinkedList', function(){
+
+  describe('Insertion', function() {
+
+    it('Simple insertion', function() {
+      var list = new DoubleLinkedList();
+      list.insert(0, new Node(5));
+      expect(list.get(0)).to.be(5);
+    });
+
+    it('Can\'t insert an invalid a value', function() {
+
+      expect(function() {
+        var list = new DoubleLinkedList();
+        list.insert(0, 5);
+      }).to.throwException();
+
+    });
+
+
+    it('Can insert multiples values', function() {
+
+      var values = [1, 2, 3, 4, 5];
+
+      var list = new DoubleLinkedList();
+      for (var i = 0; i < values.length; i++) {
+        list.insert(i, new Node(values[i]));
+      }
+
+      for (var i = 0; i < values.length; i++) {
+        expect(list.get(i)).to.be(values[i]);
+      }
+
+
+    });
+
+  });
+
+});
