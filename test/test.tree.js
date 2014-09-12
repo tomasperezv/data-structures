5d81750ad68da42fee61624a64a05395dfeb79f2
Tomas Perez <tom@0x101.com>
2014-09-13 00:33:31 +0200

    Adding more unit tests to the node-tree class.

--- test/test.tree.js
+++ test/test.tree.js
@@ -1,20 +1,29 @@
 /*global describe, it*/
 var expect = require('expect.js');
 var Tree = require('../lib/tree');
+var NodeTree = require('../lib/node-tree');
 
 describe('Tree', function(){
 
-  describe('Insertion', function() {
+  describe('Creation', function() {
 
-    it('Creation of empty trees', function() {
+    it('Empty trees', function() {
       var tree = new Tree();
       expect(tree.getRoot()).to.be(null);
     });
 
-    it('Node values must be NodeTree', function() {
+    it('Leave values must be NodeTree', function() {
       expect(function() {
         var tree = new Tree(5);
       }).to.throwException();
+    });
+
+    it('Simple creation', function() {
+      var root = new NodeTree(1, 2, 3);
+      var tree = new Tree(root);
+      expect(tree.getRoot().value()).to.be(1);
+      expect(tree.getRoot().left()).to.be(2);
+      expect(tree.getRoot().right()).to.be(3);
     });
 
   });
