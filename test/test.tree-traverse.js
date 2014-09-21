84f423dd8f39edc42a97bd3a2c338479d6b033cb
Tomas Perez <tom@0x101.com>
2014-09-21 22:33:45 +0200

    Adding in-order and post-order traverse.

--- test/test.tree-traverse.js
+++ test/test.tree-traverse.js
@@ -22,6 +22,32 @@ describe('TreeTraverse', function(){
       expect(result).eql([1, 2, 3]);
     });
 
+    it('Traverses properly a binary tree post-order', function() {
+      var root = new BinaryTreeNode(1);
+      var left = new BinaryTreeNode(2);
+      var right = new BinaryTreeNode(3);
+
+      var binaryTree = new BinaryTree(root);
+      binaryTree.setLeft(left);
+      binaryTree.setRight(right);
+      var treeTraverseAlgorithm = new TreeTraverse();
+      var result = treeTraverseAlgorithm.postorder(binaryTree.root());
+      expect(result).eql([2, 3, 1]);
+    });
+
+    it('Traverses properly a binary tree in-order', function() {
+      var root = new BinaryTreeNode(1);
+      var left = new BinaryTreeNode(2);
+      var right = new BinaryTreeNode(3);
+
+      var binaryTree = new BinaryTree(root);
+      binaryTree.setLeft(left);
+      binaryTree.setRight(right);
+      var treeTraverseAlgorithm = new TreeTraverse();
+      var result = treeTraverseAlgorithm.inorder(binaryTree.root());
+      expect(result).eql([2, 1, 3]);
+    });
+
   });
 
 });
