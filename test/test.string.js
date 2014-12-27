063c4f42a1bb6a285d07486b885381006a235d89
Tomas Perez <tom@0x101.com>
2014-12-27 19:51:17 +0100

    Adding two-sum algorithm.

---
+++ test/test.string.js
@@ -0,0 +1,30 @@
+/*global expect, require, describe, it, BFS, Tree*/
+
+var expect = require('expect.js');
+var TwoSum = require('../lib/algorithm/string/two-sum.js');
+
+describe('Two Sum', function(){
+
+  it('Basic tests', function() {
+
+    var basicTest = function(twoSumFunction) {
+      var result = twoSumFunction([1,3,5,6], 9);
+      expect(result).eql([1, 3]);
+
+      result = twoSumFunction([1,3,5,6], 10);
+      expect(result).eql(null);
+    };
+
+    describe('Solve algorithm', function() {
+      var twoSum = new TwoSum();
+      basicTest(twoSum.solve);
+    });
+
+    describe('Improved two-sum', function() {
+      var twoSum = new TwoSum();
+      basicTest(twoSum.solveImproved);
+    });
+
+  });
+
+});
