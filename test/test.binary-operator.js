f1e87ec5f89b22acdb61a33e3cd2263d59df28da
Tomas Perez <tom@0x101.com>
2014-12-29 23:21:08 +0100

    Binary operations for get, set and clear specific bits on numbers.

---
+++ test/test.binary-operator.js
@@ -0,0 +1,53 @@
+/*global expect, require, describe, it, BFS, Tree*/
+
+var expect = require('expect.js');
+var BinaryOperator = require('../lib/algorithm/binary/operators.js');
+
+describe('BinaryOperator', function(){
+
+  var testBinaryOperator = function(method, params, expected) {
+
+    var result = [];
+
+    params.map(function(currentParams) {
+      result.push(method.apply(null, currentParams));
+    });
+
+    expect(result).eql(expected);
+
+  };
+
+  describe('getBit', function() {
+
+    it('Returning bits', function() {
+      var params = [[8, 1], [8, 2], [8, 3], [8, 4], [8, 5]];
+      var expected = [false, false, false, true, false];
+      var binaryOperator = new BinaryOperator();
+      testBinaryOperator(binaryOperator.getBit, params, expected);
+    });
+
+  });
+
+  describe('setBit', function() {
+
+    it('Setting bits', function() {
+      var params = [[8, 1], [8, 2], [8, 3], [8, 4], [8, 5]];
+      var expected = [9, 10, 12, 8, 24];
+      var binaryOperator = new BinaryOperator();
+      testBinaryOperator(binaryOperator.setBit, params, expected);
+    });
+
+  });
+
+  describe('clearBit', function() {
+
+    it('Clearing bits', function() {
+      var params = [[8, 1], [8, 2], [8, 3], [8, 4], [8, 5]];
+      var expected = [8, 8, 8, 0, 8];
+      var binaryOperator = new BinaryOperator();
+      testBinaryOperator(binaryOperator.clearBit, params, expected);
+    });
+
+  });
+
+});
