018922bfa71504f7da97d0b25452ec03108a6a21
Tomas Perez <tom@0x101.com>
2014-12-30 21:20:14 +0100

    Added min-heap data structure.

---
+++ test/test.min-heap.js
@@ -0,0 +1,25 @@
+/*global describe, it*/
+var expect = require('expect.js');
+require('../lib/list');
+var MinHeap = require('../lib/min-heap');
+
+describe('MinHeap', function(){
+
+  describe('The data structure works as expected', function() {
+    it('Basic cases', function() {
+      var minHeap = new MinHeap();
+      minHeap.insert(1);
+      minHeap.insert(2);
+      minHeap.insert(-1);
+      minHeap.insert(10);
+      minHeap.insert(4);
+      expect(minHeap.extract()).to.be(-1);
+      expect(minHeap.extract()).to.be(1);
+      expect(minHeap.extract()).to.be(2);
+      expect(minHeap.extract()).to.be(4);
+      expect(minHeap.extract()).to.be(10);
+      expect(minHeap.extract()).to.be(null);
+    });
+  });
+
+});
