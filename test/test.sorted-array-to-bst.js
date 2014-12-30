34db381c77b6c42019da0f8ea9a4cfceb440b25b
Tomas Perez <tom@0x101.com>
2014-12-30 20:30:55 +0100

    Adding algorithm for converting a sorted array into a BST.

---
+++ test/test.sorted-array-to-bst.js
@@ -0,0 +1,28 @@
+/*global describe, it*/
+var expect = require('expect.js');
+require('../lib/list');
+var SortedArrayToBST = require('../lib/algorithm/sorted-array-to-bst');
+
+describe('SortedArrayToBST', function(){
+
+  describe('The algorithm works as expected', function() {
+
+    it('Basic cases', function() {
+      var A = [1, 2, 3, 4, 5, 6];
+      var sortedArrayToBST = new SortedArrayToBST();
+      var root = sortedArrayToBST.solve(A);
+      expect(root.value()).to.be(3);
+      expect(root.left().value()).to.be(1);
+      expect(root.right().value()).to.be(5);
+    });
+
+    it('Edge cases', function() {
+      var A = [];
+      var sortedArrayToBST = new SortedArrayToBST();
+      expect(sortedArrayToBST.solve([])).to.be(null);
+      expect(sortedArrayToBST.solve([1]).value()).to.be(1);
+    });
+
+  });
+
+});
