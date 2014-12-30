ceaae4866738b1501efa698de84e3b611babd3d1
Tomas Perez <tom@0x101.com>
2014-12-30 20:41:06 +0100

    Adding algorithm for computing length of largest common substring.

---
+++ test/test.largest-common-substring.js
@@ -0,0 +1,17 @@
+/*global describe, it*/
+var expect = require('expect.js');
+require('../lib/list');
+var SortedArrayToBST = require('../lib/algorithm/string/largest-common-substring');
+
+describe('LargestCommonSubstring', function(){
+
+  describe('The algorithm works as expected', function() {
+    it('Basic cases', function() {
+      var largestCommonSubstring = new LargestCommonSubstring();
+      expect(largestCommonSubstring.solve('ab', 'b')).to.be(1);
+      expect(largestCommonSubstring.solve('', 'b')).to.be(0);
+      expect(largestCommonSubstring.solve('abcdefg', 'cdefhz')).to.be(4);
+    });
+  });
+
+});
