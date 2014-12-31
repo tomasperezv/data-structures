a6c282f5ab77b0057ed8ae9c741c409d866cb6df
Tomas Perez <tom@0x101.com>
2014-12-31 19:34:44 +0100

    Implementing hash table data structure + corresponding tests.

---
+++ test/test.hash-table.js
@@ -0,0 +1,50 @@
+/*global describe, it*/
+var expect = require('expect.js');
+var HashTable = require('../lib/hash-table');
+
+describe('HashTable', function(){
+
+  describe('The data structure works as expected', function() {
+
+    it('Keys validation', function() {
+
+      var hashTable = new HashTable();
+
+      // Valid keys
+      hashTable.assertValidKey('valid');
+      hashTable.assertValidKey('validkey');
+
+      // Invalid keys
+      expect(function() {
+        hashTable.assertValidKey('');
+      }).to.throwException();
+
+      expect(function() {
+        hashTable.assertValidKey('.');
+      }).to.throwException();
+
+      expect(function() {
+        hashTable.assertValidKey('invalid-key');
+      }).to.throwException();
+
+    });
+
+    it('Insertion', function() {
+      var hashTable = new HashTable();
+      hashTable.set('keyisset', 1);
+      expect(hashTable.get('keyisset')).to.be(1);
+      expect(hashTable.get('Keyisset')).to.be(null);
+      expect(hashTable.get('keyisnotset')).to.be(null);
+
+      // Inserting a duplicated key overrides the original value
+      hashTable.set('keyisset', 2);
+      expect(hashTable.get('keyisset')).to.be(2);
+    });
+
+    it('Hash function', function() {
+      var hashTable = new HashTable();
+    });
+
+  });
+
+});
