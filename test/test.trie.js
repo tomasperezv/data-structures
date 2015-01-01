83d12253b0b5e4efc4f4e40dbe7246dcab60708f
Tomas Perez <tom@0x101.com>
2015-01-01 23:02:31 +0100

    Adding Trie data structure.

---
+++ test/test.trie.js
@@ -0,0 +1,21 @@
+/*global describe, it*/
+var expect = require('expect.js');
+var Trie = require('../lib/trie');
+
+describe('Trie', function(){
+
+  describe('Inserting and retrieval', function() {
+
+    it('Simple insertion and retrieval of words', function() {
+      var trie = new Trie();
+      trie.addWord('test');
+      trie.addWord('text');
+      trie.addWord('abcd');
+      expect(trie.getWords('te')).eql(['test', 'text']);
+      expect(trie.getWords('tes')).eql(['test']);
+      expect(trie.getWords('test')).eql(['test']);
+    });
+
+  });
+
+});
