671187a7e6cebda8a82f8e20f5aee3ca8138b235
Tomas Perez <tom@0x101.com>
2015-01-04 23:39:15 +0100

    Computing closest leaf for a given node tree.

---
+++ test/test.closest-leaf.js
@@ -0,0 +1,26 @@
+var expect = require('expect.js');
+var ClosestLeaf = require('../lib/algorithm/closest-leaf');
+var BinaryTree = require('../lib/binary-tree');
+var BinaryTreeNode = require('../lib/binary-tree-node');
+
+describe('Closest leaf', function(){
+
+  describe('Solve algorithm', function() {
+
+    it('Determine the closest leaf in a binary tree', function() {
+
+      var leftChild = new BinaryTreeNode(2);
+      var rightChild = new BinaryTreeNode(3, new BinaryTreeNode(3));
+
+      var root = new BinaryTreeNode(1, leftChild, rightChild);
+      var tree = new BinaryTree(root);
+
+      var closest = new ClosestLeaf();
+      expect(closest.solve(tree, leftChild)).eql(0);
+      expect(closest.solve(tree, rightChild)).eql(1);
+
+    });
+
+  });
+
+});
