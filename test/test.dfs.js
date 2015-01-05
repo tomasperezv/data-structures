e19b4df2cec2129f3fcc3eecebf441e520d2d598
Tomas Perez <tom@0x101.com>
2015-01-05 17:51:05 +0100

    Adding Depth First Search iterative and recursive implementations.

---
+++ test/test.dfs.js
@@ -0,0 +1,46 @@
+var expect = require('expect.js');
+var DFS = require('../lib/algorithm/dfs.js');
+var TreeNode = require('../lib/tree-node');
+var Tree = require('../lib/tree');
+
+describe('DFS', function(){
+
+  describe('Solve algorithm', function() {
+
+    /**
+     * @method generateTree
+     * @return {Tree}
+     * @public
+     */
+    var generateTree = function() {
+      var children = [new TreeNode(2), new TreeNode(3)];
+      var root = new TreeNode(1, children);
+      root.children()[0].setChildren([new TreeNode(4), new TreeNode(5)]);
+
+      return new Tree(root);
+    }
+
+    it('DFS simple search, iterative version', function() {
+
+      var tree = generateTree();
+
+      var dfs = new DFS();
+      var result = dfs.solve(tree);
+      expect(result).eql([1, 2, 4, 5, 3]);
+
+    });
+
+    it('DFS simple search, recursive version', function() {
+
+      var tree = generateTree()
+
+      var dfs = new DFS();
+      var result = dfs.solveIterative(tree.root());
+      expect(result).eql([1, 2, 4, 5, 3]);
+
+    });
+
+
+  });
+
+});
