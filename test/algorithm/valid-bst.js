86d636b288ed792e6921b6524a4a339dacf376c2
Tomás Pérez <tom@0x101.com>
2015-01-07 20:49:14 +0100

    Adding algorithm for determining if a Binary Search Tree is valid.

---
+++ test/algorithm/valid-bst.js
@@ -0,0 +1,22 @@
+var expect = require('expect.js');
+var ValidBST = require('../../lib/algorithm/valid-bst')
+var BinaryTreeNode = require('../../lib/binary-tree-node');
+var BinarySearchTree = require('../../lib/binary-search-tree');
+
+describe('Valid Binary Search Tree', function(){
+
+  describe('Solve algorithm', function() {
+
+    it('Given a Binary Search Tree, determine if it\'s valid', function() {
+
+      var root = new BinaryTreeNode(1, new BinaryTreeNode(2), new BinaryTreeNode(3));
+      var tree = new BinarySearchTree(root);
+
+      var validBST = new ValidBST();
+      expect(validBST.solve(tree)).eql(true);
+
+    });
+
+  });
+
+});
