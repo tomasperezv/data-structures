57d716fab6227fc7c3a106921b9b141ea536b68a
Tomas Perez <tom@0x101.com>
2015-01-21 23:17:10 +0100

    Adding topological ordering algorithm.

---
+++ test/algorithm/graph/test.topological-ordering.js
@@ -0,0 +1,40 @@
+var expect = require('expect.js');
+var TopologicalOrdering = require('../../../lib/algorithm/graph/topological-ordering.js');
+
+describe('Topological sorting', function(){
+
+  describe('Error handling', function() {
+
+    it('Empty graph', function() {
+      var topologicalOrdering = new TopologicalOrdering();
+      expect(topologicalOrdering.solve([])).eql([]);
+    });
+
+  });
+
+  describe('Solve algorithm', function() {
+
+    it('Topological sorting: one vertex', function() {
+      var topologicalOrdering = new TopologicalOrdering();
+      var result = topologicalOrdering.solve([[0]]);
+      expect(result).eql([0]);
+    });
+
+    it('Multiple nodes', function() {
+
+      var adjacency = [
+                      [0, 1, 1, 1, 0],
+                      [0, 0, 1, 0, 0],
+                      [0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 1],
+                      [0, 0, 0, 0, 0]
+                      ];
+
+      var topologicalOrdering = new TopologicalOrdering();
+      var result = topologicalOrdering.solve(adjacency);
+      expect(result).eql([0, 1, 2, 3, 4]);
+    });
+
+  });
+
+});
